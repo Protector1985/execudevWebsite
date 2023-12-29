fe4d04c59b7e95cb2c606394e7c5a19b338dbf4b
Protector1985 <michaelrbraun1985@gmail.com>
2023-12-28 16:58:25 -0800

    revised mobile slide-in menu styled

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -1,57 +1,59 @@
-import React, { useEffect, useRef, useState } from 'react'
-import css from './styles/styles.module.css'
-import useWidth from '@/hooks/useWidth'
-import Dropdown from 'react-bootstrap/Dropdown';
+import React, { useState } from 'react';
+import css from './styles/styles.module.css';
+import useWidth from '@/hooks/useWidth';
 import { GiHamburgerMenu } from "react-icons/gi";
 
+const NavBar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    let touchStartX = 0;
+    let touchEndX = 0;
+    const width = useWidth();
 
-const NavBar:React.FC = () => {
-
-    const width = useWidth()
-    const [show, setShow] = useState(false);
-    const dropdownRef = useRef<HTMLDivElement>(null);
-
-    const handleToggle = (isOpen:boolean) => {
-        setShow(isOpen);
+    const handleTouchStart = (e:any) => {
+        touchStartX = e.changedTouches[0].screenX;
     };
 
-    const handleFocusOut = (event:any) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.relatedTarget)) {
-            setShow(false);
+    const handleTouchMove = (e:any) => {
+        touchEndX = e.changedTouches[0].screenX;
+    };
+
+    const handleTouchEnd = () => {
+        if (touchStartX - touchEndX > 75) {
+            setIsOpen(false); // Swipe left to close
+        }
+        if (touchEndX - touchStartX > 75) {
+            setIsOpen(true); // Swipe right to open
         }
     };
-    
-
-    useEffect(() => {
-        document.addEventListener("focusout", handleFocusOut);
-        return () => {
-            document.removeEventListener("focusout", handleFocusOut);
-        };
-    }, []);
-    
+  
     return (
         <div className={css.wrapper}>
-            {width! < 768 ? 
-            <Dropdown>
-                <Dropdown.Toggle onToggle={handleToggle} ref={dropdownRef} className={css.dropdownButton}>
-                    <GiHamburgerMenu className={css.hamburgerMenu}/>
-                </Dropdown.Toggle>
-                <Dropdown.Menu className={css.mobileMenuBackground}>
-                    <Dropdown.Item className={css.dropdownItem} href="/">BLOG</Dropdown.Item>
-                    <Dropdown.Item className={css.dropdownItem} href="/about">ABOUT</Dropdown.Item>
-                    <Dropdown.Item className={css.dropdownItem} href="/contact">CONTACT</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-            
-             :
-            <>
-                <a className={css.menuItem}>BLOG</a>
-                <a className={css.menuItem}>ABOUT</a>
-                <a className={css.menuItem}>CONTACT</a>
-            </>
-        }
+            {width! < 768 ? (
+                <>
+                    <GiHamburgerMenu onClick={() => setIsOpen(!isOpen)} className={css.hamburgerMenu}/>
+                    <div 
+                        className={isOpen? css.slideIn : css.slideInClosed}
+                        onTouchStart={handleTouchStart}
+                        onTouchMove={handleTouchMove}
+                        onTouchEnd={handleTouchEnd}
+                    >
+                        <div className={css.menuMobileSub}>
+                            <a className={css.menuItemMobile}>BLOG</a>
+                            <a className={css.menuItemMobile}>ABOUT</a>
+                            <a className={css.menuItemMobile}>CONTACT</a>
+                        </div>
+                        
+                    </div>
+                </>
+            ) : (
+                <>
+                    <a className={css.menuItem}>BLOG</a>
+                    <a className={css.menuItem}>ABOUT</a>
+                    <a className={css.menuItem}>CONTACT</a>
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
