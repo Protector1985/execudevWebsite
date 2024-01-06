76a0d20ab4a9395f6e2758d1af8fd56a58a07fd6
Protector1985 <michaelrbraun1985@gmail.com>
2024-01-05 18:39:30 -0800

    google analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,28 +8,31 @@ import { useEffect } from "react";
 export default function MyApp({ Component, pageProps }: AppProps) {
   
   const router = useRouter();
-  useEffect(() => {
-    // Ensure the Google Analytics tracking ID is defined
-    const trackingId = process.env.NEXT_GOOGLE_TRACKING_ID;
+
+  const trackingId = process.env.NEXT_GOOGLE_TRACKING_ID;
     if (typeof trackingId === 'string') {
       ReactGA.initialize(trackingId);
-
-      const handleRouteChange = (url: string) => {
-        ReactGA.set({ page: url });
-        ReactGA.pageview(url);
-      };
-
-      // Track the initial pageview
-      handleRouteChange(router.asPath);
-
-      // Add event listeners for route changes
-      router.events.on('routeChangeComplete', handleRouteChange);
-
-      // Remove event listeners on cleanup
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange);
-      };
     }
+  
+  useEffect(() => {
+    // Initialize Google Analytics
+    
+
+    const handleRouteChange = (url:string) => {
+      ReactGA.set({ page: url });
+      ReactGA.pageview(url);
+    };
+
+    // Track the initial pageview
+    handleRouteChange(router.asPath);
+
+    // Add event listeners for route changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Remove event listeners on cleanup
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
   }, [router.events, router.asPath]);
 
   return <Component {...pageProps} />;
